0a122d558b5dbfe5ad4b5a8dde4640e80aee01ae
Krishitha M <122097553+Krishitha03@users.noreply.github.com>
2025-02-04 23:11:48 +0530

    Add files via upload

---
+++ Task1/ElectricCar.java
@@ -0,0 +1,8 @@
+package Task1;
+
+public class ElectricCar extends Car{
+    int CarBatteryRange;
+    public static void chargeBattery(){
+        System.out.println(CarDisplays.chargeBattery);
+    } 
+}
