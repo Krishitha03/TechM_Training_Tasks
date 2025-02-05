cf3e2aa327867ddc8025b85dc67f9e82a599b8e9
Krishitha M <122097553+Krishitha03@users.noreply.github.com>
2025-02-05 19:07:28 +0530

    Create  ExceptionHandling.java

---
+++ Week1Tasks/ExceptionHandling.java
@@ -0,0 +1,36 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ExceptionHandling {
+    //processInput method
+    public static void processInput() {
+        //putting the code in try block to catch the exception
+        try {
+            //getting input from user
+            Scanner s = new Scanner(System.in);
+            System.out.print("Enter number1: ");
+            int number1 = s.nextInt();
+            System.out.print("Enter number2: ");
+            int number2 = s.nextInt();
+
+            //to calculate reciprocal
+            int reciprocal = number2 / number1;
+
+            //printing value if no exception has occured and catched
+            System.out.println("Reciprocal value is " + reciprocal);
+        }
+        //to catch arithematic exception 
+        catch (ArithmeticException e) {
+            System.out.println("Exception occured:" + e.getMessage());
+        }
+        //to catch input mismatch exception 
+        catch (InputMismatchException e) {
+            System.out.println("Exception occured: non-numerical input");
+        }
+    }
+
+    public static void main(String[] args) {
+        //to call method
+        processInput();
+    }
+}
