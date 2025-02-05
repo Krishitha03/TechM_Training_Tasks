ae559cb9cc5da65d247355cee22473b4ba6e4bc7
Krishitha M <122097553+Krishitha03@users.noreply.github.com>
2025-02-05 18:56:02 +0530

    Delete OOPS directory

--- OOPS/CarDisplays.java
+++
@@ -1,5 +0,0 @@
-
-public class CarDisplays {
-    public static final String engineStarted="The Engine has started.";
-    public static final String chargeBattery="The battery is being charged";
-}
