0b7b2edf2ef641084a569b85ea5e4c2ba404058d
Krishitha M <122097553+Krishitha03@users.noreply.github.com>
2025-02-05 19:45:38 +0530

    Update DaysOfWeek.java

--- Week1Tasks/Enum/DaysOfWeek.java
+++ Week1Tasks/Enum/DaysOfWeek.java
@@ -2,6 +2,7 @@
 public class DaysOfWeek {
     public static void main(String[] args) {
         for (Enum_DaysOfWeek day : Enum_DaysOfWeek.values()) {
+            //check condition equal to saturday or sunday for holiday else not
             if (day == Enum_DaysOfWeek.SATURDAY || day == Enum_DaysOfWeek.SUNDAY)
                 System.out.println(day + " is Holiday");
             else
