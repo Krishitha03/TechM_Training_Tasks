981c3e03ff5302b4207a42428a10979b64093d89
Krishitha M <122097553+Krishitha03@users.noreply.github.com>
2025-02-05 19:11:41 +0530

    Create FinalKeyword.java

---
+++ Week1Tasks/Final keyword/FinalKeyword.java
@@ -0,0 +1,18 @@
+package Task4;
+
+public class FinalKeyword {
+    
+    public static void main(String[] args) {
+        String name="Krishitha";
+        final int age;
+      //variable age value cannot be changed because it is declared with final keyword
+        // age++;
+        age=25;
+        // age=26;
+        System.out.println(name+"is"+age+"years old");
+    }
+  //FinalMethod cannot be overriden
+    public static final void FinalMethod(){
+        System.out.println("from inherited method");
+    }
+}
