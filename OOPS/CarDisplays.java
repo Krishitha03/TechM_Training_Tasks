b4adce14e75942d6c89b9a95da9aa8291ac83fb8
Krishitha M <122097553+Krishitha03@users.noreply.github.com>
2025-02-05 18:54:50 +0530

    Add files via upload

---
+++ OOPS/CarDisplays.java
@@ -0,0 +1,5 @@
+
+public class CarDisplays {
+    public static final String engineStarted="The Engine has started.";
+    public static final String chargeBattery="The battery is being charged";
+}
