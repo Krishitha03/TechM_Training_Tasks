426fbefcc39fa342439f5ac730ec53be749c10ab
Krishitha03 <krishitham646@gmail.com>
2025-03-04 23:27:04 +0530

    updated

--- Week5Tasks/GameHavenStore/src/components/Footer.tsx
+++ Week5Tasks/GameHavenStore/src/components/Footer.tsx
@@ -5,13 +5,7 @@ const Footer = () => {
   return (
     <Box bg="gray.800" color="white" py={6} mt={8}>
       <Container maxW="container.lg">
-        <Stack direction={["column", "row"]} justify="space-between" align="center">
-          {/* Navigation Links */}
-          <HStack spacing={6}>
-            <Link href="/" _hover={{ textDecoration: "underline" }}>Home</Link>
-            <Link href="/about" _hover={{ textDecoration: "underline" }}>About</Link>
-            <Link href="/contact" _hover={{ textDecoration: "underline" }}>Contact</Link>
-          </HStack>
+        <Stack direction="column" align="center" spacing={4}>
 
           {/* Social Media Icons */}
           <HStack spacing={4}>
@@ -19,12 +13,13 @@ const Footer = () => {
             <IconButton as="a" href="https://github.com" aria-label="GitHub" icon={<FaGithub />} />
             <IconButton as="a" href="https://linkedin.com" aria-label="LinkedIn" icon={<FaLinkedin />} />
           </HStack>
-        </Stack>
 
-        {/* Copyright */}
-        <Text textAlign="center" mt={4} fontSize="sm">
-          © {new Date().getFullYear()} Your Website Name. All rights reserved.
-        </Text>
+          {/* Copyright */}
+          <Text textAlign="center" fontSize="sm">
+            © {new Date().getFullYear()} GameHaven. All rights reserved.
+          </Text>
+          
+        </Stack>
       </Container>
     </Box>
   );
